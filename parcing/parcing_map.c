4d9b909d37886297a85ccca4ad3974ca7cdf1f55
Raphael Perrin <rperrin@c3r1p6.42nice.fr>
2023-04-16 17:42:47 +0200

    CHECK WALLS

--- parcing/parcing_map.c
+++ parcing/parcing_map.c
@@ -6,7 +6,7 @@
 /*   By: rperrin <rperrin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/15 02:43:34 by rperrin           #+#    #+#             */
-/*   Updated: 2023/04/16 01:47:26 by rperrin          ###   ########.fr       */
+/*   Updated: 2023/04/16 17:42:23 by rperrin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,33 +35,31 @@ int	ft_strcmp(char *check, char *str)
 	return (0);
 }
 
+vod
+
 int	check_walls_cut(t_data *data, int i, int j)
 {
-	// printf("[%c]", data->map[j + 1][i]);
-	// printf("[%c]", data->map[j][i + 1]);
-	// printf("[%c]", data->map[j - 1][i]);
-	// printf("[%c]\n\n", data->map[j][i - 1]);
-	// printf("VALIDE %d\n", ft_strcmp("0NSWE ", &data->map[j + 1][i]));
-	// printf("PAS VALIDE %d\n", ft_strcmp("0NSWE ", &data->map[j][i - 1]));
-	if (data->map[j + 1][i] && data->map[j - 1][i] && ft_strcmp(\
-	"1", &data->map[j + 1][i]) == 0 && ft_strcmp("1", &data->map[j - 1][i]))
+	if ('0' == data->map[j][i + 1])
 	{
-		printf("COUCOU\n");
-		if (!data->map[j][i + 1] || !data->map[j][i - 1])
-			return (1);
-		if ((data->map[j][i + 1] && data->map[j][i - 1] ) && ( ft_strcmp("0NSWE "\
-		, &data->map[j][i + 1]) && ft_strcmp("0NSWE ", &data->map[j][i - 1]) ))
-			return (1);
+		data->map[j][i + 1] = 'X';
+		check_walls_cut(data, i + 1, j);
+	}
+	if ('0' == data->map[j][i - 1])
+	{
+		data->map[j][i - 1] = 'X';
+		check_walls_cut(data, i - 1, j);
+	}
+	if ('0' == data->map[j - 1][i])
+	{
+		data->map[j - 1][i] = 'X';
+		check_walls_cut(data, i, j - 1);
+	}
+	if ('0' == data->map[j + 1][i])
+	{
+		data->map[j + 1][i] = 'X';
+		check_walls_cut(data, i, j + 1);
 	}
 	return (0);
-	// if (data->map[j - 1][i] && ft_strcmp("1", &data->map[j - 1][i]))
-
-	// if (data->map[j][i + 1] && ft_strcmp("0NSWE ", &data->map[j][i + 1]))
-	// 	return (1);
-	// 	return (1);
-	// if (data->map[j][i - 1] && ft_strcmp("0NSWE ", &data->map[j - 1][i]))
-	// 	return (1);
-	// return (0);
 }
 
 int	check_walls(t_data	*data)
@@ -75,14 +73,14 @@ int	check_walls(t_data	*data)
 	{
 		while (data->map[j][i])
 		{
-			//printf("TEST j = %d i = %d [%c]\n", j, i, data->map[j][i]);
-			if (data->map[j][i++] == '0')
+			if (data->map[j][i] == 'N')
 			{
-				if (check_walls_cut(data, 4, 1))
-				{
-					printf("PAS VALIDE\n");
-				}
+				printf("coucou [%c]\n\n", data->map[j][i]);
+				check_walls_cut(data, i, j);
+				// check_walls_3000(data, i, j);
+				print_map(data->map);
 			}
+			i++;
 		}
 		printf("\n");
 		i = 0;
